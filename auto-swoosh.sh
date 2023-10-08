b5f5ce07d30a89cb929db2bc61f0496c4a456467
Andreas Zeller <zeller@cispa.de>
2023-10-08 13:07:51 +0200

    New: use -x for debugging output

--- auto-swoosh.sh
+++ auto-swoosh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Play Mail Sent "Swoosh" sound even if do not disturb is on
 
-# uncomment to get a log
-# set -x
+# Use -x as option to get diagnostic messages
+if [ "$1" = '-x' ]; then
+    set -x
+fi
 
 # The sound to be played - 'Mail Sent' (swoosh)
 mail_sent_sound='/System/Applications/Mail.app/Contents/Resources/Mail Sent.aiff'
@@ -13,13 +15,16 @@ exec log stream --predicate '(subsystem == "com.apple.mail" and (composedMessage
 (
     while true; do
         read LINE
-        case $LINE in
+        LINE="$LINE" # for debugging output
+        case "$LINE" in
         *Filtering\ the\ log\ data*)
             ;;
         *About\ to\ play*)
             sending=true
             ;;
         *sound\ play\ aborted*)
+            # This message also occurs for incoming mail on DnD, 
+            # so we only play a sound if we have seen a "sending" message
             if $sending; then
                 afplay "$mail_sent_sound"
             fi
